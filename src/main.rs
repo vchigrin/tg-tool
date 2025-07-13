7d672b22cfc3c41f18612afd11e49af0b7ea91b0
Vyacheslav Chigrin <vyacheslav.chigrin@izba.dev>
2025-07-13 17:58:16 +0300

    Add command-line parsing skeleton.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use clap::{Parser, Subcommand};
+use eyre::Result;
+use std::path;
+
+#[derive(Debug, Parser)]
+#[command(version, about = "Tool for command-line interaction with Telegram")]
+struct CliParams {
+    #[command(subcommand)]
+    command: Command,
+    session_file: path::PathBuf,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    #[command(subcommand)]
+    Folders(FoldersCommand),
+    Login,
+    Logout,
+}
+
+#[derive(Debug, Subcommand)]
+enum FoldersCommand {
+    Backup { dst_file_path: path::PathBuf },
+    Restore { src_file_path: path::PathBuf },
+}
+
+fn do_main() -> Result<()> {
+    let params = CliParams::parse();
+    println!("Running with params {params:?}");
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match do_main() {
+        Ok(_) => {
+            println!("Success")
+        }
+        Err(e) => {
+            println!("Error {e}");
+            std::process::exit(1);
+        }
+    }
 }
